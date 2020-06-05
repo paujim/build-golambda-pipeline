518e75b38c34036a5f1a4eb397ed3748be82fca1
paujim <paujim@hotmail.com>
2020-06-06 08:10:26 +1000

    update pipeline

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 
 CODECOMMIT_REPO_NAME = "pj-secrets"
-ARTIFACT_BUCKET_NAME = "pj-artifacts"
-LAMBDA_KEY = "secretlambda.zip"
+ARTIFACT_BUCKET_NAME = "pj-artifacts-in-oregon"
+LAMBDA_KEY = "runSQL.zip"
 
 GITHUB = "paujim"
-GITHUB_REPO = "secrethelper"
+GITHUB_REPO = "runSQL"
 
 SECRET_GITHUB_ID = "github/oAuthToken"
-SECRET_GITHUB_JSON_FIELD="oAuthToken"
+SECRET_GITHUB_JSON_FIELD = "oAuthToken"
--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -13,110 +13,103 @@ from aws_cdk import (
 )
 
 
-class RepoStack(core.Stack):
-
+class PipelineStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # lambda_repository = codecommit.Repository(
-        #     scope=self,
-        #     id="CODECOMIT-LAMBDA-REPO",
-        #     repository_name=constants.CODECOMMIT_REPO_NAME,
-        # )
-
         artifact_bucket = s3.Bucket(
             scope=self,
-            id="S3-ARTIFACT",
-            bucket_name=constants.ARTIFACT_BUCKET_NAME
-        )
-        self.artifact_bucket_name = artifact_bucket.bucket_name
-
-
-class PipelineStack(core.Stack):
-    def __init__(self, scope: core.Construct, id: str, artifact_bucket_name: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
-
-        # lambda_repository = codecommit.Repository.from_repository_name(
-        #     scope=self,
-        #     id="IMPORTED-REPO",
-        #     repository_name=constants.CODECOMMIT_REPO_NAME,
-        # )
-
-        artifact_bucket = s3.Bucket.from_bucket_name(
-            scope=self,
-            id="ARTIFACT-BUCKET",
-            bucket_name=artifact_bucket_name,
+            id="s3-artifact",
+            bucket_name=constants.ARTIFACT_BUCKET_NAME,
+            removal_policy=core.RemovalPolicy.DESTROY,
         )
 
-        # Codebuild
-        # codecommit_source = codebuild.Source.code_commit(
-        #     repository=lambda_repository,
-        #     branch_or_ref="refs/heads/master",
-        # )
-
-        github_source = codebuild.Source.git_hub(
-            owner=constants.GITHUB,
-            repo=constants.GITHUB_REPO,
-            branch_or_ref="refs/heads/master",
-        )
         lambda_build = codebuild.Project(
             scope=self,
-            id="LAMBDA-BUILD",
-            source=github_source,
-            artifacts=codebuild.Artifacts.s3(
-                identifier="artifact",
-                bucket=artifact_bucket,
-                encryption=False,
-                path="codebuild_output",
-                name="lambda.zip"),
+            id="codebuild-build",
+            build_spec=codebuild.BuildSpec.from_object({
+                "version": "0.2",
+                "env": {
+                    "variables": {
+                        "ENV_NAME": "pj",
+                        "GO111MODULE": "on",
+                    }
+                },
+
+                "phases":
+                {
+                    "install": {
+                        "commands": [
+                            "go get .",
+
+                        ]
+                    },
+                    "pre_build": {
+                        "commands": [
+                            "go vet .",  # Check the Go code for common problems with 'go vet'
+                            "go test .",  # Run all tests included with our application
+                        ]
+                    },
+                    "build": {
+                        "commands": [
+                            "go build -o main",  # Build the go application
+                            "zip main.zip main",
+                        ]
+                    }
+                },
+                "artifacts": {
+                    "files": ["main.zip"],
+                    "name": "$ENV_NAME-lambda-$(date +%Y-%m-%d).zip"
+                }
+
+            }
+            ),
         )
 
         oauth_token = core.SecretValue.secrets_manager(
             secret_id=constants.SECRET_GITHUB_ID,
             json_field=constants.SECRET_GITHUB_JSON_FIELD,
         )
+
         # Codepipeline
+        lambda_pipeline = codepipeline.Pipeline(
+            scope=self,
+            id="lambda-pipeline",
+            # artifact_bucket=artifact_bucket,
+        )
+
         source_output = codepipeline.Artifact()
+        lambda_pipeline.add_stage(
+            stage_name="Source",
+            actions=[
+                codepipeline_actions.GitHubSourceAction(
+                    oauth_token=oauth_token,
+                    action_name="GitHub",
+                    owner=constants.GITHUB,
+                    repo=constants.GITHUB_REPO,
+                    output=source_output,
+                )]
+        )
+
         build_output = codepipeline.Artifact()
-
-        # source_action = codepipeline_actions.CodeCommitSourceAction(
-        #     action_name="CodeCommit",
-        #     repository=lambda_repository,
-        #     output=source_output
-        # )
-
-        source_action = codepipeline_actions.GitHubSourceAction(
-            oauth_token=oauth_token,
-            action_name="GitHub",
-            owner=constants.GITHUB,
-            repo=constants.GITHUB_REPO,
-            output=source_output,
+        lambda_pipeline.add_stage(
+            stage_name="Build",
+            actions=[codepipeline_actions.CodeBuildAction(
+                action_name="CodeBuild",
+                project=lambda_build,
+                input=source_output,
+                outputs=[build_output]
+            )]
         )
-        build_action = codepipeline_actions.CodeBuildAction(
-            action_name="CodeBuild",
-            project=lambda_build,
-            input=source_output,
-            outputs=[build_output]
-        )
+
         s3_action = codepipeline_actions.S3DeployAction(
             bucket=artifact_bucket,
             input=build_output,
             action_name="S3Upload",
-            extract=False,
+            extract=True,
             object_key=constants.LAMBDA_KEY,
         )
-        lambda_pipeline = codepipeline.Pipeline(
-            scope=self,
-            id="LAMBDA-PIPELINE",
-        )
-        lambda_pipeline.add_stage(
-            stage_name="Source",
-            actions=[source_action]
-        )
-        lambda_pipeline.add_stage(
-            stage_name="Build",
-            actions=[build_action]
-        )
+
         lambda_pipeline.add_stage(
             stage_name="Upload",
             actions=[s3_action]
